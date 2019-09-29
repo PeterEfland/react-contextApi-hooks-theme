117bec60e11a57c71340d99e3358fd5bbdf91989
Peter <phefland@gmail.com>
2019-09-29 19:16:45 -0300

    Refactoring LanguageContext to be a functional component with createContext and useState hooks.

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -1,24 +1,13 @@
-import React, { Component, createContext } from "react";
+import React, { createContext, useState } from "react";
 
 export const LanguageContext = createContext();
 
-export class LanguageProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { language: "portuguese" };
-  }
-
-  changeLanguage = e => {
-    this.setState({ language: e.target.value });
-  };
-
-  render() {
-    return (
-      <LanguageContext.Provider
-        value={{ ...this.state, changeLanguage: this.changeLanguage }}
-      >
-        {this.props.children}
-      </LanguageContext.Provider>
-    );
-  }
+export function LanguageProvider(props) {
+  const [language, setLanguage] = useState("portuguese");
+  const changeLanguage = e => setLanguage(e.target.value);
+  return (
+    <LanguageContext.Provider value={{ language, changeLanguage }}>
+      {props.children}
+    </LanguageContext.Provider>
+  );
 }
