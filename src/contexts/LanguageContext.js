f926b7fb1db502983b0906cd5dd7497806af3dc0
Peter <phefland@gmail.com>
2019-09-29 19:07:11 -0300

    turning Navbar from class to functional component and using useContext hook to consume 2 contexts without needing higher order component. Much easier!

--- src/contexts/LanguageContext.js
+++ src/contexts/LanguageContext.js
@@ -22,9 +22,3 @@ export class LanguageProvider extends Component {
     );
   }
 }
-
-export const withLanguageContext = Component => props => (
-  <LanguageContext.Consumer>
-    {value => <Component languageContext={value} {...props} />}
-  </LanguageContext.Consumer>
-);
