a8a7b55e5063fc4706844fbe4d097a1129431b62
Peter <phefland@gmail.com>
2019-09-29 19:28:37 -0300

    Refactor ThemeContext to functional component with createContext hook and custom hook to toggle state.

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,24 +1,14 @@
-import React, { Component, createContext } from "react";
+import React, { createContext } from "react";
+import useToggleState from "../hooks/useToggleState";
 
 export const ThemeContext = createContext();
 
-export class ThemeProvider extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isDarkMode: false };
-  }
+export function ThemeProvider(props) {
+  const [isDarkMode, toggleTheme] = useToggleState(false);
 
-  toggleTheme = () => {
-    this.setState({ isDarkMode: !this.state.isDarkMode });
-  };
-
-  render() {
-    return (
-      <ThemeContext.Provider
-        value={{ ...this.state, toggleTheme: this.toggleTheme }}
-      >
-        {this.props.children}
-      </ThemeContext.Provider>
-    );
-  }
+  return (
+    <ThemeContext.Provider value={{ isDarkMode, toggleTheme }}>
+      {props.children}
+    </ThemeContext.Provider>
+  );
 }
